e4d934626f78640a67496e915f91bba1a1239a42
Eddy Valencia <eddyvalencia8@gmail.com>
2017-11-12 19:26:59 -0400

    Fixed: Administrador puede cambiar clave de usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -99,17 +99,21 @@ class UsuarioController extends Controller
      */
     public function actionUpdate($id)
     {
-
         $model = $this->findModel($id);
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
+        $request = crypt(Yii::$app->request->post('Users')['password'], Yii::$app->params["salt"]);
+        $model->password = $request;
+        if(Yii::$app->request->post()){
+            $array = array_merge(array(), Yii::$app->request->post());
+            $array["Users"]["password"] = $request;
+            if ($model->load($array) && $model->save()) {
+              return $this->redirect(['view', 'id' => $model->id]);
+            }
+        }else {
             return $this->render('update', [
                 'model' => $model,
             ]);
         }
     }
-
     /**
      * Deletes an existing Users model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
